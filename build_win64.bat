777c356cf7b849d7368e06abf5f9953c79c3b510
Wang Hao <991060@gmail.com>
2024-10-31 17:43:18 +0800

    Fix win64 build command
    - The SDK is now compiled as static libs to avoid malloc crash caused by using stl templates across dll boundary

--- build_win64.bat
+++ build_win64.bat
@@ -1,5 +1,18 @@
+mkdir build
 pushd build
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install/win64 -DENABLE_TESTING=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_STANDARD=17 -DCUSTOM_MEMORY_MANAGEMENT=ON -DBUILD_ONLY="cognito-idp"
+
+cmake ../ ^
+-DCMAKE_BUILD_TYPE=Release ^
+-DCMAKE_INSTALL_PREFIX=../install/win64 ^
+-DENABLE_UNITY_BUILD=ON ^
+-DENABLE_TESTING=OFF ^
+-DFORCE_SHARED_CRT=ON ^
+-DBUILD_SHARED_LIBS=OFF ^
+-DCMAKE_CXX_STANDARD=20 ^
+-DCPP_STANDARD=20 ^
+-DBUILD_ONLY="cognito-idp"
+
 cmake --build . --config=Release
 cmake --install . --config=Release
+
 popd
